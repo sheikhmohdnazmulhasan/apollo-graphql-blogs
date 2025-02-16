eddbe611735bd8a35b3c30117a5a9d5859841e68
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2025-02-16 22:47:00 +0600

    Define data set

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,19 @@ const typeDefs = `#graphql
     title: String
     author: String
   }
-  
+
   type Query {
     books: [Book]
   }
 `;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
