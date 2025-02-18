734694d993bc88c2758fc17d95c650b8695ea007
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2025-02-18 23:20:08 +0600

    add logger for logging

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import logger from "./utils/logger";
 
 const typeDefs = `#graphql
   type Book {
@@ -30,14 +31,20 @@ const resolvers = {
 };
 
 (async function () {
-  console.log("Starting the server...");
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
+  try {
+    logger.info("Starting the Apollo Server...");
 
-  console.log(`🚀  Server ready at: ${url}`);
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: 4000 },
+    });
+
+    logger.info(`🚀 Server is running at: ${url}`);
+  } catch (error) {
+    logger.error(`❌ Error starting server: ${error}`);
+  }
 })();
