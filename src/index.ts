3e3745952297f17672f416cc8c80dc3aee87ad48
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2025-02-19 23:32:42 +0600

    relocate the schema

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import logger from "./utils/logger";
-
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
+import { typeDefs } from "./schema";
 
 const books = [
   {
