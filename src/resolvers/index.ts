49d750ae7c98adadc3ec935e917395a3382c8f22
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2025-02-21 22:30:32 +0600

    hash the password before save into db

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,5 @@
 import prisma from "../db/prisma";
+import bcrypt from "bcryptjs";
 
 export const resolvers = {
   // This is the Query resolver
@@ -12,8 +13,13 @@ export const resolvers = {
   Mutation: {
     // This is the resolver for the createUser mutation
     createUser: async (parent: any, args: any, context: any, info: any) => {
+      const hashedPassword = await bcrypt.hash(args.password, 10);
+
       return await prisma.user.create({
-        data: args,
+        data: {
+          password: hashedPassword,
+          ...args,
+        },
       });
     },
   },
